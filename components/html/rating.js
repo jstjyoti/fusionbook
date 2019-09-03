e5742b6f71e99b5a1e624406603137ea0eb4bac5
Jyoti Mishra <jyotimishra29091997@gmail.com>
2019-09-03 14:34:30 +0530

    much stories added

--- components/html/rating.js
+++ components/html/rating.js
@@ -1,53 +1,147 @@
-
-function positions(pWidth, pHeight,i,j){
-  let side = Math.min(pWidth, pHeight),
-  str = "m" + ((pWidth/ 2)+(i*side)) + "," + ((pHeight - side)/2+(j*side)),
-  ax = 0.15, 
-  bx = (1 - 2 * ax)/2, 
-  cx = 0.5, 
-  ay = 0.3, by = 0.25, 
-  cy = (1 - ay - by), 
-  dy = 0.3;
-  str += " l" + (ax * side) + "," + (ay * side)
-  + " h" + (bx * side)
-  + " l-" + (2*ax * side) + "," + (by * side)
-  + " l" + (2*ax * side) + "," + (cy * side)
-  + " l-" + (cx * side) + ",-" + (dy * side)
-  + " l-" + (cx * side) + "," + (dy * side)
-  + " l" + (2*ax * side) + ",-" + (cy * side)
-  + " l-" + (2*ax * side) + ",-" + (by * side)
-  + " h" + (bx * side)
-  + " z"; 
-  return str;
+var N 
+var rating=1
+function calculatePositions(pWidth, pHeight, strokeWidth, unit, xShift = 0, yShift = 0, direction = 'row',N=1){
+    if(unit == '%'){
+        //strokeWidth = Math.min(pWidth, pHeight) * (strokeWidth / 25);
+        throw new TypeError('Must provide stroke width in pixels');
+    }
+    let h = direction == 'row' ? pWidth / N : pWidth;
+    let w = direction == 'column' ? pHeight / N : pHeight; 
+    let side = Math.min(h, w) - Math.max(strokeWidth * 4, 4),
+    str = "M" + (xShift + (pWidth / 2)) + "," + (yShift + (pHeight - side) / 2),
+    ax = 0.15, 
+    bx = (1 - 2 * ax)/2, 
+    cx = 0.3,
+    dx = 0.5, 
+    ex = 0.3,
+    ay = 0.3, by = 0.3, 
+    cy = (1 - ay - by), 
+    dy = 0.25,
+    am = ax / ay;
+    cx = (am * cy);
+    ex = ex * am;
+    str += " l" + (ax * side) + "," + (ay * side);
+    str += " h" + (bx * side);
+    str += " l-" + (cx * side) + "," + (by * side);
+    str += " l" + (cx * side) + "," + (cy * side);
+    str += " l-" + (dx * side) + ",-" + (dy * side);
+    str += " l-" + (dx * side) + "," + (dy * side);
+    str += " l" + (cx * side) + ",-" + (cy * side);
+    str += " l-" + (cx * side) + ",-" + (by * side);
+    str += " h" + (bx * side);
+    str += " z"; 
+    return str;
 }
 
-export default function star(pElement, width, height, styles,direction){
-  let svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-  svg.setAttribute("height", height);
-  svg.setAttribute("width", width);
-  
-  var b=function (){
-    for(var i=0;i<5;i++)
-    {
-      var j=0;
-      let path = document.createElementNS("http://www.w3.org/2000/svg", "path")
-      var str1 = '';
-      if(direction=="column")
-      {
-        path.setAttribute("d", positions((width/5), height,i,j));
-      }
-      else if(direction=="row"){
-        path.setAttribute("d",positions(width,(height/5),j,i))
-      }
-      for(let a in styles){
-          str1 += a + ':' + styles[a] + ';';
-      }
-      path.setAttribute("style",str1);
-      svg.appendChild(path);
-
+function adjustSize(size){
+    let num = (size+'').match(/\d+/g),
+    unit = (size+'').match(/px|%|vh|vw/g) || [''];
+    if(!num){
+        throw new TypeError("error in size value");
+    }
+    return{
+        num: +num[0],
+        unit: unit[0]
     };
-
-  pElement.appendChild(svg);
-  }();
 }
 
+
+export default function cStar(pElement, width=200, height=200, direction, styles,N=1,rating=1){
+    let svg = document.createElementNS("http://www.w3.org/2000/svg", "svg"),
+    stars = [],
+    styleStrR = 'fill:"#000" ; stroke:"#fff";',
+    styleStrU = 'fill:"#ddd" ; stroke:"#fff";',
+    xShift = 0, yShift = 0,
+    strokeWidth = styles["stroke-width"] ? adjustSize(styles["stroke-width"]) : 2;
+
+    if(!strokeWidth.num){
+        strokeWidth = {
+            "num": 2,
+            "unit": ''
+        }
+    }
+
+    console.log(strokeWidth.num);
+
+    if(!(pElement instanceof HTMLElement)){
+        throw new TypeError("First argument must be a html element");
+    }
+
+    for(let i = 0; i < N; i++){
+        stars.push(document.createElementNS("http://www.w3.org/2000/svg", "path"));
+    }
+    if(height<=1||!+height)
+      console.error("Incorrect Height");
+    if(width<=1||!+width)
+      console.error("Incorrect width");
+      
+    svg.setAttribute("height", height);
+    svg.setAttribute("width", width);
+    pElement.appendChild(svg);
+
+    // for(let i in customAttributes){
+    //     if(styles.hasOwnProperty(customAttributes[i].styleName)){
+    //         if(!styles[customAttributes[i].styleName].match(customAttributes[i].regex)){
+    //             throw new TypeError(customAttributes[i].err);
+    //         }
+    //     }
+    // }
+
+    if(strokeWidth.num > Math.min(svg.clientWidth, svg.clientHeight) / (2*N)){
+        console.log(strokeWidth.num, svg.clientWidth, svg.clientHeight);
+        throw new TypeError("Incorrect value of stroke width");
+    }
+
+    if(direction == 'row'){
+        if(styles["content"] == "stretch"){
+            xShift = svg.clientWidth / N;
+        }else{
+            xShift = Math.min(svg.clientWidth / N, svg.clientHeight);
+        }
+    }else if(direction == 'column'){
+        if(styles["content"] == "stretch"){
+            yShift = svg.clientHeight / N;
+        }else{ 
+            // if(styles["content"] == "center")
+            yShift = Math.min(svg.clientHeight / N, svg.clientWidth);
+        }
+    }else{
+        throw new TypeError('Incorrect direction');
+    }
+
+    for(let i = 0; i < stars.length; i++){
+      
+        stars[i].setAttribute("d", calculatePositions(svg.clientWidth, svg.clientHeight, strokeWidth.num, strokeWidth.unit, xShift * ((Math.floor(N/2)) - i), yShift * ((Math.floor(N/2)) - i), direction,N))
+    }
+
+    // for(let i in allowedStyles){
+    //     if(styles.hasOwnProperty(allowedStyles[i].styleName)){
+    //         if(styles[allowedStyles[i].styleName].match(allowedStyles[i].regex)){
+    //             styleStr += allowedStyles[i].styleName + ':' + styles[allowedStyles[i].styleName] + ';';
+    //         }else{
+    //             throw new TypeError(allowedStyles[i].err);
+    //         }
+    //     }
+    // }
+    for(let a in styles){
+      if(a=="rated")
+      {
+        for(let p in styles[a])
+        styleStrR += p + ':' + styles[a][p] + ';';
+      }
+      if(a=="unrated")
+      {
+        for(let p in styles[a])
+        styleStrU += p + ':' +  styles[a][p] + ';';
+      }
+
+    }
+    for(let i = 0; i < stars.length; i++){
+      if(i<Math.ceil(rating))
+        stars[i].setAttribute("style", styleStrR);
+      else
+        stars[i].setAttribute("style", styleStrU);
+      svg.appendChild(stars[i]);
+    }
+    pElement.appendChild(svg);
+}
--- stories/rating.js
+++ stories/rating.js
@@ -1,35 +1,367 @@
 import { Story, notes, configs } from '../src/lib/story'
-import star from '../components/html/rating'
+import cStar from '../components/html/rating'
 
 const ratingStory = new Story('Rating').addMetas([configs()])
-
 ratingStory.addChapter(
-  'vertical',
+  'Default Single star',
   story => {
-    star(story,200, 200, {
-      "fill": "blue",
-      "stroke": "red",
-      "stroke-width": "1",
-      "stroke-linejoin":"miter" 
-    },"row") 
+    cStar(story, "400px", "100px", 'row', {
+      "rated" :{"fill": "#fff",
+        "stroke": "#000"
+      }
+    },1,1)
   },
   [
-    notes('This is vertical square stars.')
+    notes('Single square star')
   ]
 )
 
 ratingStory.addChapter(
-  'horizontal',
+  'Single star with particular(blue here) fill color',
   story => {
-    star(story,200, 200, {
-      "fill": "blue",
-      "stroke": "red",
-      "stroke-width": "1",
-      "stroke-linejoin":"miter" 
-    },"column")
+    cStar(story, "400px", "100px", 'row', {
+      "rated":{
+       "fill": "blue",
+        "stroke": "#000"
+      }
+    },1,1)
   },
   [
-    notes('This is horizontal square stars.')
+    notes('Single square star having fill color blue')
+  ]
+)
+
+ratingStory.addChapter(
+  'stroke-width testing',
+  story => {
+    cStar(story, "500px", "400px", 'row', {
+        "rated":{"fill": "blue",
+        "stroke": "#000",
+        "stroke-width": "20px"
+      }
+    },1,1)
+  },
+  [
+    notes('star won\'t be deformed even for thicker stroke-width' )
+  ]
+)
+ratingStory.addChapter(
+  'stroke-color testing',
+  story => {
+    cStar(story, "500px", "400px", 'row', {
+      "rated":{
+        "fill": "blue",
+        "stroke": "red",
+        "stroke-width": "5px"
+      }
+    },1,1)
+  },
+  [
+    notes('star with any particular Stroke color')
+  ]
+)
+ratingStory.addChapter(
+  'svg with single-star having height-width %',
+  story => {
+    cStar(story, "50%", "60%", 'row', {
+        "rated":{"fill": "#fff",
+        "stroke": "#000",
+        "stroke-width":"2"
+        }
+    },1,1)
+  },
+  [
+    notes('Height-width of svg in %')
+  ]
+)
+
+ratingStory.addChapter(
+  'svg with 0 height-width testing',
+  story => {
+   
+    cStar(story,0,0, 'row', {
+        "rated":{"fill": "#fff",
+        "stroke":"#000",
+        "stroke-width":"2"
+    }
+  },1,1)
+  },
+  [
+    notes('svg with missing params later i can add a check for style as well and go with missing params inside style object')
+  ]
+)
+
+
+ratingStory.addChapter(
+  'svg with incorrect height-width testing',
+  story => {
+    debugger
+    cStar(story,"abc","xyz", 'row', {
+      "rated":{"fill": "#fff",
+      "stroke":"#000",
+      "stroke-width":"2"
+      }
+    },1,1)
+  },
+  [
+    notes('svg with wrong height width ,error message recieved ..so user must provide correct numeric values ')
+  ]
+)
+
+ratingStory.addChapter(
+  'svg with fraction height width testing',
+  story => {
+    debugger
+    cStar(story,100.2,200.25, 'row', {
+      "rated":{"fill": "#fff",
+      "stroke":"#000",
+      "stroke-width":"2"
+      }
+    },1,1)
+  },
+  [
+    notes('svg with fractional values of height width gives correct result')
+  ]
+)
+
+ratingStory.addChapter(
+  'svg with missing parameters(height-width) in cStar',
+  story => {
+    
+    cStar(story, 'row', {
+      "rated":{"fill": "#fff",
+      "stroke":"#000",
+      "stroke-width":"2"
+      }
+    },1,1)
+    
+  },
+  [
+    notes('svg with missing params later i can add a check for style as well and go with missing params inside style object')
+  ]
+)
+
+ratingStory.addChapter(
+  'Any number of star testing',
+  story=>{
+    cStar(story,"200","200",{
+      "rated":{"fill": "#fff",
+      "stroke":"#000",
+      "stroke-width":"5"
+      }
+    },20,20)
+  },
+  [
+    notes('svg with any Number of star')
+  ]
+)
+
+ratingStory.addChapter(
+  'Wrong-direction testing',
+  story=>{
+    cStar(story,"200","200","abc",{
+    rated:{"fill":"#fff",
+      "stroke":"000",
+      "stroke-width":"5"
+    }
+    },2,2)
+  },
+  [
+    notes("direction isn\'t row or column")
+  ]
+
+)
+
+ratingStory.addChapter(
+  'Any other wrong style information',
+  story=>{
+    cStar(story,"200","200","row",{
+      "rated":{"fill":"abc",
+      "stroke":"000",
+      "stroke-width":"5",
+      "float":"left"
+    }
+    },1,1)
+  },
+  [
+    notes('star with wrong styles must show invalid Style Provided message to user')
+  ]
+)
+
+ratingStory.addChapter(
+  'Horizontal rating',
+  story => {
+    cStar(story, "400px", "100px", 'row', {
+    "rated":{"fill": "#ff0",
+      "stroke": "#000"
+    } ,
+    "unrated":{"fill": "#ddd",
+    "stroke": "#000"
+    }
+    },5,4)
+  },
+  [
+    notes('Horizontal five-star rating showing stars horizontally')
+  ]
+)
+
+
+ratingStory.addChapter(
+  'Vertical rating',
+  story => {
+    cStar(story, "400px", "100px", 'column', {
+      "rated":{"fill": "blue",
+      "stroke": "#000"
+    } ,
+    "unrated":{"fill": "purple",
+    "stroke": "#000"
+    }
+    },5,4)
+  },
+  [
+    notes('Vertical five-star rating showing stars vertically')
+  ]
+)
+
+ratingStory.addChapter(
+  'width and height svg in percentage testing',
+  story => {
+    cStar(story, "50%", "50%", 'row', {
+      "rated":{"fill": "blue",
+      "stroke": "#000"
+    } ,
+    "unrated":{"fill": "purple",
+    "stroke": "#000"
+    }
+    },5,5)
+  },
+  [
+    notes('Horizontal five-star rating showing stars horizontally where width and height is in %')
+  ]
+)
+ratingStory.addChapter(
+  'width and height in percentage vertical testing',
+  story => {
+    cStar(story, "50%", "50%", 'column', {
+      "rated":{"fill": "blue",
+      "stroke": "#000"
+    } ,
+    "unrated":{"fill": "purple",
+    "stroke": "#000"
+    }
+    },5,5)
+  },
+  [
+    notes('vertical five-star rating showing stars horizontally where width is in %')
+  ]
+)
+
+ratingStory.addChapter(
+  'Justify Content stretch testing',
+  story => {
+    cStar(story, "1000px", "100px", 'row', {
+      "rated":{"fill": "blue",
+      "stroke": "#000"
+    } ,
+    "unrated":{"fill": "purple",
+    "stroke": "#000"
+    }
+    },3,3)
+  },
+  [
+    notes('Horizontal rating showing stars automatic adjust space between stars')
+  ]
+)
+
+ratingStory.addChapter(
+  'Justify Content center testing',
+  story => {
+    cStar(story, "1000px", "100px", 'row', {
+      "rated":{"fill": "blue",
+      "stroke": "#000"
+    },
+    "unrated":{"fill": "purple",
+    "stroke": "#000"
+    }
+    },3,3)
+  },
+  [
+    notes('Horizontal rating showing stars center with required space only')
+  ]
+)
+ratingStory.addChapter(
+  'default no style provided rating vertical-testing',
+  story => {
+    cStar(story, "400px", "100px", 'column', {
+
+    },5,4.5)
+  },
+  [
+    notes('five-star rating with different color showing stars vertically')
+  ]
+)
+
+ratingStory.addChapter(
+  'default no style provided rating vertical-testing',
+  story => {
+    cStar(story, "400px", "100px", 'row', {
+
+    },5,4.5)
+  },
+  [
+    notes('five-star rating with different color showing stars horizontally')
+  ]
+)
+
+
+ratingStory.addChapter(
+  'color-variable rating vertical-testing',
+  story => {
+    cStar(story, "400px", "100px", 'column', {
+      "rated":{"fill": "blue",
+      "stroke": "#000"
+    } ,
+    "unrated":{"fill": "purple",
+    "stroke": "#000"
+    }
+    },5,4.5)
+  },
+  [
+    notes('five-star rating with different color showing stars vertically')
+  ]
+)
+
+ratingStory.addChapter(
+  'color-variable rating Horizontal testing',
+  story => {
+    cStar(story, "400px", "100px", 'row', {
+      "rated":{"fill": "#ff0",
+       "stroke": "#000"
+     } ,
+     "unrated":{"fill": "#fff",
+     "stroke": "#000"
+     },
+   },5,3.5)
+  },
+  [
+    notes('five-star rating with different color showing stars horizontal')
+  ]
+)
+
+ratingStory.addChapter(
+  'stroke-variable in rating vertical testing',
+  story => {
+    cStar(story, "400px", "100px", 'column', {
+      "rated":{"fill": "blue",
+      "stroke": "green"
+    } ,
+    "unrated":{"fill": "purple",
+    "stroke": "blue"
+    }
+    },5,4)
+  },
+  [
+    notes('five-star rating with different color showing stars horizontal')
   ]
 )
 
